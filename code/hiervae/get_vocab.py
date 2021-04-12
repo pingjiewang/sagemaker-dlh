d8b6fb303dbf5e2bfca1c5c3dc989be7f2fb727f
aijun2 <aijun2@illinois.edu>
2021-04-12 09:34:26 -0400

    to add more logs

--- code/hiervae/get_vocab.py
+++ code/hiervae/get_vocab.py
@@ -4,6 +4,7 @@ from rdkit import Chem
 from multiprocessing import Pool
 
 def process(data):
+	print("peter: begin process() data: ", data)
     vocab = set()
     for line in data:
         s = line.strip("\r\n ")
@@ -13,15 +14,19 @@ def process(data):
             vocab.add( attr['label'] )
             for i,s in attr['inter_label']:
                 vocab.add( (smiles, s) )
+	print("vocab: ", vocab)
+	print(len(vocab))
     return vocab
 
 if __name__ == "__main__":
-
+	print("peter: begin get_vocab()")
     data = [mol for line in sys.stdin for mol in line.split()[:2]]
+	print("data: ", data)
     data = list(set(data))
-
+	print("list data: ", data)
     ncpu = 8
     batch_size = len(data) // ncpu + 1
+	print(ncpu, len(data), batch_size)
     batches = [data[i : i + batch_size] for i in range(0, len(data), batch_size)]
 
     pool = Pool(ncpu)
