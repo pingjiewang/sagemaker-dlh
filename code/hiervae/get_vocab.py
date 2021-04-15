b86cb88ce00fd762195e2db85d0fccaf19912880
Ubuntu <ubuntu@ip-172-31-42-75.us-east-2.compute.internal>
2021-04-15 00:19:11 -0400

    update train_pairs file

--- code/hiervae/get_vocab.py
+++ code/hiervae/get_vocab.py
@@ -4,7 +4,7 @@ from rdkit import Chem
 from multiprocessing import Pool
 
 def process(data):
-    print("peter: begin process() data: ", data)
+    #print("peter: begin process() data: ", data)
     vocab = set()
     for line in data:
         s = line.strip("\r\n ")
@@ -14,19 +14,19 @@ def process(data):
             vocab.add( attr['label'] )
             for i,s in attr['inter_label']:
                 vocab.add( (smiles, s) )
-    print("vocab: ", vocab)
-    print(len(vocab))
+    #print("vocab: ", vocab)
+    #print(len(vocab))
     return vocab
 
 if __name__ == "__main__":
-    print("peter: begin get_vocab()")
+    #print("peter: begin get_vocab()")
     data = [mol for line in sys.stdin for mol in line.split()[:2]]
-    print("data: ", data)
+    #print("data: ", data)
     data = list(set(data))
-    print("list data: ", data)
-    ncpu = 8
+    #print("list data: ", data)
+    ncpu = 15
     batch_size = len(data) // ncpu + 1
-    print(ncpu, len(data), batch_size)
+    #print(ncpu, len(data), batch_size)
     batches = [data[i : i + batch_size] for i in range(0, len(data), batch_size)]
 
     pool = Pool(ncpu)
