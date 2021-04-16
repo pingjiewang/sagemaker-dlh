15ecb4941debe201eb73d26325132b80dfe4206d
pingjiewang <ping.wang@eagletechva.com>
2021-04-15 22:13:48 -0400

    Fixed hie training script error in pre-proccessed data path

--- code/hiervae/train_qed.sh
+++ code/hiervae/train_qed.sh
@@ -10,7 +10,7 @@
 
 # QED 
 mkdir -p newmodels/qed
-python gnn_train.py --train processed_qed/ --vocab data/qed/vocab.txt --save_dir newmodels/qed/ --hidden_size 300 --anneal_rate 0.9 --epoch 20 --batch_size 20 --beta 2.0 --latent_size 8 | tee newmodels/qed/LOG
+python gnn_train.py --train data/qed/ --vocab data/qed/vocab.txt --save_dir newmodels/qed/ --hidden_size 300 --anneal_rate 0.9 --epoch 20 --batch_size 20 --beta 2.0 --latent_size 8 | tee newmodels/qed/LOG
 
 # # DRD2 
 # mkdir -p newmodels/drd2
