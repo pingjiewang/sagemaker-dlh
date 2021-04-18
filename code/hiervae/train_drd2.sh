a51249138a7db1013485a188dfe3e1aa1e4d1eaf
aijun2 <aijun2@illinois.edu>
2021-04-18 16:36:08 -0400

    put preprocess under modlel name

--- code/hiervae/train_drd2.sh
+++ code/hiervae/train_drd2.sh
@@ -2,4 +2,4 @@
 
 # DRD2 
 mkdir -p newmodels/drd2
-python gnn_train.py --train data/drd2/ --vocab data/drd2/vocab.txt --save_dir newmodels/drd2/ --hidden_size 300 --anneal_rate 0.9 --epoch 20 --batch_size 20 --beta 2.0 --latent_size 8 | tee newmodels/drd2/LOG
+python gnn_train.py --train data/drd2/processed/ --vocab data/drd2/vocab.txt --save_dir newmodels/drd2/ --hidden_size 300 --anneal_rate 0.9 --epoch 20 --batch_size 20 --beta 2.0 --latent_size 8 | tee newmodels/drd2/LOG
