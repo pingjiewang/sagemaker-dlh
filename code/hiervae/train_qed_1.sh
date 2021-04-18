a8971b0c85eac4918aa1f662ac049552b1300e3f
peter zhang <peterzhangon@gmail.com>
2021-04-18 16:18:13 -0400

    Update train_qed_1.sh
    put preprocessed data to /data/qed/processed/

--- code/hiervae/train_qed_1.sh
+++ code/hiervae/train_qed_1.sh
@@ -2,4 +2,4 @@
 
 # QED 
 mkdir -p newmodels/qed
-python gnn_train.py --train ./processed_qed/ --vocab data/qed/vocab.txt --save_dir newmodels/qed/ --hidden_size 300 --anneal_rate 0.9 --epoch 1 --batch_size 20 --beta 2.0 --latent_size 8 | tee newmodels/qed/LOG
+python gnn_train.py --train /data/qed/processed/ --vocab data/qed/vocab.txt --save_dir newmodels/qed/ --hidden_size 300 --anneal_rate 0.9 --epoch 1 --batch_size 20 --beta 2.0 --latent_size 8 | tee newmodels/qed/LOG
