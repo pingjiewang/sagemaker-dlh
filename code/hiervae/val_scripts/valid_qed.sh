2b73e6d6bef92be4f558bab78c80757b4cd371dd
aijun2 <aijun2@illinois.edu>
2021-04-20 23:09:50 -0400

    to fix the bedsize and wrong path issues

--- code/hiervae/val_scripts/valid_qed.sh
+++ code/hiervae/val_scripts/valid_qed.sh
@@ -8,6 +8,6 @@ for ((i=ST; i<=ED; i++)); do
     f=$DIR/model.$i
     if [ -e $f ]; then
         echo $f
-        python decode.py --test data/qed/valid.txt --vocab data/qed/vocab.txt --model $f --hidden_size 300 --embed_size 300 | python ../scripts/qed_score.py > $DIR/results.$i &
+        python decode.py --test data/qed/valid.txt --vocab data/qed/vocab.txt --model $f --hidden_size 300 --embed_size 300 | python scripts/qed_score.py > $DIR/results.$i &
     fi
 done
