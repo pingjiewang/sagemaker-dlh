915d3213f9269198586d8b573b459f2dd946ea7b
aijun2 <aijun2@illinois.edu>
2021-04-22 21:53:52 -0400

    to update the script for validation

--- code/hiervae/val_scripts/valid_drd2-new.sh
+++ code/hiervae/val_scripts/valid_drd2-new.sh
@@ -9,6 +9,6 @@ for ((i=ST; i<=ED; i++)); do
     f=$DIR_MODEL/model.$i
     if [ -e $f ]; then
         echo $f
-        python decode.py --test ../data/molopt/drd2/valid.txt --vocab ../data/molopt/drd2/align_vocab.txt --enum_root --model $f --hidden_size 300 --embed_size 300 --latent_size 8 | sed -r '/^\s*$/d' | python ../scripts/drd2_score.py > $DIR_OUT/results.$i 
+        python decode.py --test data/drd2/valid.txt --vocab data/drd2/vocab.txt --model $f --hidden_size 300 --embed_size 300 --latent_size 8 | sed -r '/^\s*$/d' | python scripts/drd2_score.py > $DIR_OUT/results.$i 
     fi
 done
