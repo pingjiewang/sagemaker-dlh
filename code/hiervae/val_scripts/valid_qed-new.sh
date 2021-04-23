c6593b96bb04e1bf7e4702f72a41f7528d566381
aijun2 <aijun2@illinois.edu>
2021-04-23 00:07:22 -0400

    to update the validation script for hie

--- code/hiervae/val_scripts/valid_qed-new.sh
+++ code/hiervae/val_scripts/valid_qed-new.sh
@@ -10,9 +10,9 @@ echo "begin to validate qed from $ST to $ED."
 for ((i=ST; i<=ED; i++)); do
     f=$DIR_MODEL/model.$i
     if [ -e $f ]; then
-        echo $f
+        echo "Begin to process $f."
         python decode.py --test data/qed/valid.txt --vocab data/qed/vocab.txt --model $f --hidden_size 300 --embed_size 300 --latent_size 8| sed -r '/^\s*$/d' | python scripts/qed_score.py > $DIR_OUT/results.$i 
-	echo "[python decode.py --test data/qed/valid.txt --vocab data/qed/vocab.txt --model $f --hidden_size 300 --embed_size 300 --latent_size 8| sed -r '/^\s*$/d' | python scripts/qed_score.py > $DIR_OUT/results.$i]"
+        echo "[python decode.py --test data/qed/valid.txt --vocab data/qed/vocab.txt --model $f --hidden_size 300 --embed_size 300 --latent_size 8| sed -r '/^\s*$/d' | python scripts/qed_score.py > $DIR_OUT/results.$i]"
     fi
 echo "**********\n\n "
 done
